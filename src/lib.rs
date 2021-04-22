4bf4702db6ea75158e099f559e38f1f5ac0d3c66
Eric <31060229+erihsu@users.noreply.github.com>
2021-04-22 20:49:24 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use serde_json::Value;
 use std::error::Error;
 
-pub trait PdkPlugin {
-    // given model name , get clk pin input capacitance
-    fn login(&mut self, username: &str, password: &str) -> CTSPluginRes<()>;
-    fn logout(&mut self) -> CTSPluginRes<()>;
-    fn get_sink_cap(&self, name: &str) -> CTSPluginRes<f32>;
-    // given model name and placement, get its clk pin offset. The offset is mapping between orient code and coords
-    // refer orient code to vulcan/src/parser/encoder.rs
-    fn get_sink_clk_pin_offset(&self, name: &str) -> CTSPluginRes<HashMap<i8,(f32, f32)>>;
-    // given model name, get buffer detailed information which includes power and timing
-    fn get_buffer(&self, name: &str) -> CTSPluginRes<Value>;
-    // list all avaliale buffer for clock tree synthesis
-    fn list_all_clock_buffer(&self) -> CTSPluginRes<Vec<String>>;
-    // get metal layer unit resistance
-    fn get_unit_resistance(&self) -> CTSPluginRes<f32>; // fF
-    // get metal layer unit capacitance
-    fn get_unit_capacitance(&self) -> CTSPluginRes<f32>; // kohm
-}
+// pub trait PdkPlugin {
+//     // given model name , get clk pin input capacitance
+//     fn login(&mut self, username: &str, password: &str) -> CTSPluginRes<()>;
+//     fn logout(&mut self) -> CTSPluginRes<()>;
+//     fn get_sink_cap(&self, name: &str) -> CTSPluginRes<f32>;
+//     // given model name and placement, get its clk pin offset. The offset is mapping between orient code and coords
+//     // refer orient code to vulcan/src/parser/encoder.rs
+//     fn get_sink_clk_pin_offset(&self, name: &str) -> CTSPluginRes<HashMap<i8,(f32, f32)>>;
+//     // given model name, get buffer detailed information which includes power and timing
+//     fn get_buffer(&self, name: &str) -> CTSPluginRes<Value>;
+//     // list all avaliale buffer for clock tree synthesis
+//     fn list_all_clock_buffer(&self) -> CTSPluginRes<Vec<String>>;
+//     // get metal layer unit resistance
+//     fn get_unit_resistance(&self) -> CTSPluginRes<f32>; // fF
+//     // get metal layer unit capacitance
+//     fn get_unit_capacitance(&self) -> CTSPluginRes<f32>; // kohm
+// }
 
 pub trait DesignPlugin {
     // login
