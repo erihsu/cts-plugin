28c67f6be80aed6c87e4688cdc6510e7b387b5b2
Eric <31060229+erihsu@users.noreply.github.com>
2021-04-24 17:44:26 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,14 @@ pub trait CTSPlugin {
     fn login(&mut self, username: &str, password: &str) -> CTSPluginRes<()>;
     // logout
     fn logout(&mut self) -> CTSPluginRes<()>;
-    // given clock network name, return a set of sinks. The information include sink name(component), sink location(clk pin) and sink capacitance
-    fn get_clock_sinks(&self, clk: &str) -> CTSPluginRes<Vec<(String, (i32, i32), f32)>>;
-    // get clock input source, aka. CLK PIN
-    fn get_clock_source(&self, clk: &str) -> CTSPluginRes<(i32, i32)>;
-    // given clock net name, clock net Route Definition, change the internal design
-    fn add_clock_net(&mut self, net_name: &str, net: &Vec<Route>,from:String,to:String) -> CTSPluginRes<()>;
+    // given clock network name, return a set of sinks. The information include sink name, sink location
+    fn get_clock_sinks(&self, clk: &str) -> CTSPluginRes<Vec<(String, (i32, i32))>>;
+    // given clock net name, clock net Route Definition, source component name, sink component name, change the internal design
+    fn add_clock_net(&mut self, net: &[Path],from:(String,(i32,i32)),to:Vec<(String,(i32,i32))>) -> CTSPluginRes<String>;
     // given buffer model, buffer location and orientation, change the internal design
-    fn add_clock_buffer(
-        &mut self,
-        model: &str,
-        location: (i32, i32),
-        orient: i8,
-    ) -> CTSPluginRes<String>;
+    fn add_intermediate_buffer(&mut self,model: &str,location: (i32, i32)) -> CTSPluginRes<String>;
+    // add root buffer. this will automatically add the net between root buffer and clock source.
+    fn add_root_buffer(&mut self,model:&str,location:(i32,i32)) -> CTSPluginRes<String>;
     // export standard def file
     fn export_def(&self, path: &str) -> CTSPluginRes<()>;
     // import standard def file to load design , make sure first prepare layer map and via map
@@ -39,22 +34,8 @@ pub trait CTSPlugin {
 
 pub type CTSPluginRes<T> = Result<T, Box<dyn Error>>;
 
-pub struct Route<'a> {
-    pub layer: &'a str,
-    pub element: Vec<Element<'a>>,
-}
-
-pub enum Element<'a> {
-    Path(Path),
-    Via(Via<'a>),
-}
-
 pub struct Path {
     pub from: (i32, i32),
+    pub turn: Option<(i32,i32)>,
     pub to: (i32, i32),
-}
-
-pub struct Via<'a> {
-    pub coord: (i32, i32),
-    pub vname: &'a str,
 }
